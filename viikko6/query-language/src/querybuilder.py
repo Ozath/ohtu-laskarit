026d3f9a0f6bf3d3be0085c9cfcc680ac512e10b
Ozath <ttkorkki2@gmail.com>
2021-12-12 22:35:37 +0200

    W6E4

---
+++ viikko6/query-language/src/querybuilder.py
@@ -0,0 +1,18 @@
+from matchers import All, And, HasAtLeast, HasFewerThan, HasAtLeast, Or, PlaysIn
+
+class QueryBuilder:
+
+    def __init__(self, query=All()):
+        self.query = query
+    
+    def build(self):
+        return self.query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(PlaysIn(team), self.query))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(HasAtLeast(value, attr), self.query))
+        
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(HasFewerThan(value, attr), self.query))
